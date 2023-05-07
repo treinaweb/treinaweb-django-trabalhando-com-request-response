db848ae0b51ad5f34a39abc082aabdd3128eb9e7
Cleyson Lima <cleysonph@gmail.com>
2023-05-07 17:19:44 -0300

    02:03 - Entendendo o fluxo de request e response do Django

--- app/views.py
+++ app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpRequest, HttpResponse
 
 
-def home(request):
-    return render(request, 'app/home.html')
+def home(request: HttpRequest) -> HttpResponse:
+    # body = loader.render_to_string("app/home.html", context=None, request=request)
+    template = loader.get_template("app/home.html")
+    body = template.render(context=None, request=request)
+    return HttpResponse(body)

4b904d5fc487124c418ff3574bf60e0e5bf6601b
Cleyson Lima <cleysonph@gmail.com>
2023-05-07 19:36:54 -0300

    02:09 - Retornando arquivos

--- app/views.py
+++ app/views.py
@@ -1,20 +1,16 @@
-import json
-from django.shortcuts import redirect
-from django.template import loader
-from django.http import HttpRequest, HttpResponse, HttpResponseRedirect, JsonResponse
+from django.http import HttpRequest, HttpResponse, FileResponse
 from django.views.decorators.csrf import csrf_exempt
 
 
 @csrf_exempt
 def home(request: HttpRequest) -> HttpResponse:
-    # body = json.dumps({"nome": "Cleyson", "sobrenome": "Lima"})
-    # return HttpResponse(content=body, content_type="application/json", charset="utf-8")
-    body = [
-        {"nome": "Cleyson", "sobrenome": "Lima"},
-        {"nome": "Ana", "sobrenome": "Beatriz"},
-        {"nome": "Elton", "sobrenome": "Fonseca"}
-    ]
-    return JsonResponse(body, safe=False)
+    # with open('relatorio.pdf', 'rb') as file:
+    #     return HttpResponse(
+    #         content=file.read(), 
+    #         content_type="application/pdf",
+    #         headers={"Content-Disposition": "attachment; filename='relatorio.pdf'"}
+    #     )
+    return FileResponse(open('relatorio.pdf', 'rb'), as_attachment=True, filename='outro_nome.pdf')
 
 
 def outra_view(request: HttpRequest) -> HttpResponse:

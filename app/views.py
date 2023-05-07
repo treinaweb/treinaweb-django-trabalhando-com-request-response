32cd7850eda0f3a2b1393ddc965b1eb60e8061a0
Cleyson Lima <cleysonph@gmail.com>
2023-05-07 19:02:32 -0300

    02:07 - Trabalhando com redirecionamento

--- app/views.py
+++ app/views.py
@@ -1,10 +1,14 @@
+from django.shortcuts import redirect
 from django.template import loader
-from django.http import HttpRequest, HttpResponse, HttpResponseNotAllowed
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
 
 
 @csrf_exempt
 def home(request: HttpRequest) -> HttpResponse:
-    if request.method == "GET":
-        return HttpResponse("<h1>Tudo certo!</h1>")
-    return HttpResponseNotAllowed(permitted_methods=["GET"])
+    # return HttpResponseRedirect("http://localhost:8000/outra-rota")
+    return redirect("outra_rota")
+
+
+def outra_view(request: HttpRequest) -> HttpResponse:
+    return HttpResponse(content="<h1>Outra rota</h1>")
--- setup/urls.py
+++ setup/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from app.views import home
+from app.views import home, outra_view
 
 urlpatterns = [
     path('', home, name='home'),
     path('xpto', home, name='home2'),
+    path('outra-rota', outra_view, name='outra_rota'),
     path('admin/', admin.site.urls),
 ]

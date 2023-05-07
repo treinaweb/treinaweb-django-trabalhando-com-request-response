e1aeca8ae88da21a3558ce907a2447d5d1f42d1a
Cleyson Lima <cleysonph@gmail.com>
2023-05-07 19:11:42 -0300

    02:08 - Retornando JSON

--- app/views.py
+++ app/views.py
@@ -1,13 +1,20 @@
+import json
 from django.shortcuts import redirect
 from django.template import loader
-from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
 
 @csrf_exempt
 def home(request: HttpRequest) -> HttpResponse:
-    # return HttpResponseRedirect("http://localhost:8000/outra-rota")
-    return redirect("outra_rota")
+    # body = json.dumps({"nome": "Cleyson", "sobrenome": "Lima"})
+    # return HttpResponse(content=body, content_type="application/json", charset="utf-8")
+    body = [
+        {"nome": "Cleyson", "sobrenome": "Lima"},
+        {"nome": "Ana", "sobrenome": "Beatriz"},
+        {"nome": "Elton", "sobrenome": "Fonseca"}
+    ]
+    return JsonResponse(body, safe=False)
 
 
 def outra_view(request: HttpRequest) -> HttpResponse:

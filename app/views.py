30f98d6f49f45bd9b7e8ac76d43ebd2e4f20e87f
Cleyson Lima <cleysonph@gmail.com>
2023-05-07 18:06:55 -0300

    02:04 - Obtendo metadados da requisição

--- app/views.py
+++ app/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpRequest, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 def home(request: HttpRequest) -> HttpResponse:
     # body = loader.render_to_string("app/home.html", context=None, request=request)
     template = loader.get_template("app/home.html")
     body = template.render(context=None, request=request)
+    print(f"Path: {request.path}")
+    print(f"Path info: {request.path_info}")
+    print(f"Method: {request.method}")
+    print(f"Content type: {request.content_type}")
+    print(f"Header: {request.headers}")
+    # print(f"META: {request.META}")
+    print(f"Session: {request.session.items()}")
+    print(f"User: {request.user}")
+    print(f"Host: {request.get_host()}")
+    print(f"Port: {request.get_port()}")
+    print(f"Full path: {request.get_full_path()}")
+    print(f"Full path info: {request.get_full_path_info()}")
+    print(f"Is secure: {request.is_secure()}")
+    print(f"Accepts: {request.accepts('application/json')}")
     return HttpResponse(body)
--- setup/urls.py
+++ setup/urls.py
@@ -5,5 +5,6 @@ from app.views import home
 
 urlpatterns = [
     path('', home, name='home'),
+    path('xpto', home, name='home2'),
     path('admin/', admin.site.urls),
 ]

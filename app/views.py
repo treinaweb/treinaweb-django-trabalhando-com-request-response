22ea76ed278f4c87dbeec59db829033ab2e1a43c
Cleyson Lima <cleysonph@gmail.com>
2023-05-07 18:49:25 -0300

    02:06 - Subclasses de HttpResponse

--- app/views.py
+++ app/views.py
@@ -1,18 +1,10 @@
-import json
-from django.shortcuts import render
 from django.template import loader
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseNotAllowed
 from django.views.decorators.csrf import csrf_exempt
 
 
 @csrf_exempt
 def home(request: HttpRequest) -> HttpResponse:
-    # body = loader.render_to_string("app/home.html", context=None, request=request)
-    template = loader.get_template("app/home.html")
-    body = template.render(context=None, request=request)
-    print(f"GET: {request.GET}")
-    print(f"POST: {request.POST}")
-    print(f"Body: {request.body}")
-    body_dict = json.loads(request.body)
-    print(body_dict)
-    return HttpResponse(body)
+    if request.method == "GET":
+        return HttpResponse("<h1>Tudo certo!</h1>")
+    return HttpResponseNotAllowed(permitted_methods=["GET"])
